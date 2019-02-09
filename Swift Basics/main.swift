64f48842a9ea6595dd992845d4bba5c52331af43
Park <jcpfmc@mwc-025156.dhcp.missouri.edu>
2019-02-08 19:48:17 -0600

    First commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Park, Joy (MU-Student) on 2/8/19.
+//  Copyright © 2019 Park. All rights reserved.
+//
+
+// gives access to foundation library
+import Foundation
+
+var name: String = "Joy"
+var age: Int = 20
+var value: Double = 154521
+var payRate = 134000.51
+
+print("\(name) is \(age) years old")
+
+for i in 1...5 {
+    print("i = \(i)")
+}
